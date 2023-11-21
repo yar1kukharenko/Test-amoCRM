5822a49f655750bf25e7dfaca53cfe11be1d86bd
Iaroslav Kukharenko <113698528+yar1kukharenko@users.noreply.github.com>
2023-11-22 02:38:26 +0300

    Add footer, begin main

--- build/js/index.bundle.js
+++
@@ -1,1 +0,0 @@
-!function(){"use strict";!function(){const o=document.querySelector(".mobile-nav-btn"),e=document.querySelector(".mobile-nav"),n=document.querySelector(".nav-icon");o.onclick=function(){e.classList.toggle("mobile-nav--open"),n.classList.toggle("nav-icon--active"),document.body.classList.toggle("no-scroll")}}()}();
